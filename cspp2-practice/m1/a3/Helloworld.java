754ba2732602988d185c93c94a9ff4e7a2449d01
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-27 21:51:46 +0530

    hello commit{

---
+++ cspp2-practice/m1/a3/Helloworld.java
@@ -0,0 +1,25 @@
+/**
+
+    * Author: Anisha Vulli.
+    * Date: 27th Aug
+
+*/
+public final class Helloworld {
+
+    /*Public class*/
+    /**
+       * Constructor.
+    */
+    private Helloworld() {
+    // Empty constructor
+    }
+
+    /**
+    *
+    * @param args of type String
+    */
+    private static void main(final String[] args) {
+        /*Main function*/
+        System.out.println("Hello World!");
+    }
+}
