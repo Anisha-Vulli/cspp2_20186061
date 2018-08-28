3c92767e6b9b4df1bc3c3e3d7674cf9ff9dff490
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-28 20:13:47 +0530

    "check_style_score -> 3 of 5 passed. style score: 0/1 "

---
+++ cspp2-practice/m2/Assignment-3/Solution.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    static int power(int base, int exponent) {
+        if (exponent == 1) {
+            return base;
+        }
+        else {
+            return base * power(base, exponent - 1);
+        }
+    }
+    
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int base = s.nextInt();
+        int exponent = s.nextInt();
+        Solution obj = new Solution();
+        int result = obj.power(base,exponent);
+        System.out.println(result);
+    }
+    /*
+    Need to write the power function and print the output.
+    */
+}
