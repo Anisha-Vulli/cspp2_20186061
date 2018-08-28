1250244e100e183d9fb4b53b78122c83d37d7e5d
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-28 11:40:35 +0530

    area commit

---
+++ cspp2-practice/m1/Assignment-2/Solution.java
@@ -0,0 +1,43 @@
+/**
+
+    * Author: Anisha Vulli.
+    * Date: 28th Aug 2018
+
+*/
+import java.util.Scanner;
+/**
+  * Calculating area of the triangle.
+*/
+public final class Solution {
+    /*Public class*/
+    /**
+       * Constructor.
+    */
+
+    private Solution() {
+    // Empty constructor.
+    }
+
+    /**
+        * @param args of type String
+    */
+    public static void main(final String[] args) {
+        /**
+           * Main function.
+        */
+        final int base, height;
+        // System.out.println("Enter base and height values");
+        Scanner scan = new Scanner(System.in);
+        base = scan.nextInt();
+        height = scan.nextInt();
+        Solution tri = new Solution();
+        System.out.println(tri.areaOfTriangle(base, height));
+    }
+
+    public double areaOfTriangle(final int base, final int height) {
+        // Area of triangle function
+        final double a = 0.5;
+        double area = a * base * height;
+        return area;
+    }
+}
