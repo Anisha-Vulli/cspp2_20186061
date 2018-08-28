bb0694bdb1477ea2578cf2c2e5e1f03329e3159c
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-28 20:20:15 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-practice/m2/Assignment-3/Solution.java
+++ cspp2-practice/m2/Assignment-3/Solution.java
@@ -1,8 +1,24 @@
+/**
+ * Author : Anisha Vulli.
+ * Date : 28th Aug 2018.
+ */
+
 import java.util.Scanner;
+/**
+ * Class for solution.
+ */
 
 public class Solution {
+    /**
+     * Power calculation function
+     *
+     * @param      base      The base
+     * @param      exponent  The exponent
+     *
+     * @return     value
+     */
 
-    static long power(int base, int exponent) {
+    static long power(final int base, final int exponent) {
         if (exponent == 0) {
             return 1;
         }
@@ -10,16 +26,23 @@ public class Solution {
             return base * power(base, exponent - 1);
         }
     }
-    
-    public static void main(String[] args) {
+    /**
+     * Constructs the object.
+     */
+    private Solution(){
+        //Empty constructor
+    }
+    /**
+     * Main function
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
         Scanner s=new Scanner(System.in);
-        int base = s.nextInt();
-        int exponent = s.nextInt();
+        final int base = s.nextInt();
+        final int exponent = s.nextInt();
         Solution obj = new Solution();
         long result = obj.power(base,exponent);
         System.out.println(result);
     }
-    /*
-    Need to write the power function and print the output.
-    */
 }
