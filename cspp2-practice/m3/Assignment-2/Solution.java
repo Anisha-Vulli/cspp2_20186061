6358951f8d4b3397f86ac4259ba9809fcc548977
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-29 12:14:48 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-practice/m3/Assignment-2/Solution.java
+++ cspp2-practice/m3/Assignment-2/Solution.java
@@ -7,24 +7,28 @@ import java.util.Scanner;
  * Class for solution.
  */
 public class Solution {
-	/**
-	 * Main function.
-	 *
-	 * @param      args  The arguments
-	 */
-    public static void main(String[] args) {
+    /**
+     * Main Function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
         
-        Scanner s = new Scanner(System.in);      
+        Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         int count = 0;
         for (int i = 0; i <= n; i++) {
-        	int num = i;
-        	while(num != 0) {
-        		if (num % 10 == 7) {
-        			count = count + 1;
-        		}
-        		num = num / 10;
-        	}
+            int num = i;
+            final int zero = 0;
+            final int ten = 10;
+            final int seven = 7;
+            final int one = 1;
+            while (num != zero) {
+                if (num % ten == seven) {
+                    count = count + one;
+                }
+                num = num / ten;
+            }
         }
         System.out.println(count);
        
