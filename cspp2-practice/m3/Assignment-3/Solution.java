20539fec634c5687e3af0dff6df599c1d5f74f7f
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-29 11:52:43 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-practice/m3/Assignment-3/Solution.java
+++ cspp2-practice/m3/Assignment-3/Solution.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-public class Solution {
+public final class Solution {
     /**
      * GCD calculation function.
      *
@@ -18,7 +18,7 @@ public class Solution {
     static int gcd(final int n1, final int n2) {
         int min = Math.min(n1, n2);
         int minval = min + 1;
-        for (int i = minval; i >= 1; i-- ) {
+        for(int i = minval; i >= 1; i-- ) {
             if ((n1 % i == 0) && (n2 % i == 0)) {
                 return (i);
             }
@@ -28,20 +28,17 @@ public class Solution {
 
     }
     /**
-     * Main function
+     * Main function.
      *
      * @param      args  The arguments
      */
 
     public static void main(final String[] args) {
 
-        Scanner s = new Scanner(System.in);      
+        Scanner s = new Scanner(System.in);
         int n1 = s.nextInt();
         int n2 = s.nextInt();
         Solution obj = new Solution();
-        System.out.println(obj.gcd(n1,n2));
+        System.out.println(obj.gcd(n1, n2));
     }
-    /*
-	Need to write the gcd function and print the output.
-	*/
 }
