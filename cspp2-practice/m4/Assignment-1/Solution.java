5c47f0df3cf358c0e79d96766454875bc9596b19
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-30 14:43:49 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-practice/m4/Assignment-1/Solution.java
+++ cspp2-practice/m4/Assignment-1/Solution.java
@@ -18,10 +18,9 @@ public final class Solution {
      *
      * @param      args  The arguments
      */
-    public static void main(final String[] args)
-    {
+    public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n= sc.nextInt();
+        int n = sc.nextInt();
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
