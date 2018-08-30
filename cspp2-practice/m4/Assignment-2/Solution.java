b3ff95932080f261664acf9c02390618cc55e57c
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-30 15:34:13 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-practice/m4/Assignment-2/Solution.java
+++ cspp2-practice/m4/Assignment-2/Solution.java
@@ -7,46 +7,52 @@ import java.util.Scanner;
  * Class for solution.
  */
 public class Solution {
-	/**
-	 * Main function.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+    /**
+     * Main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int rows1 = sc.nextInt();
         int coloumn1 = sc.nextInt();
         int[][] mat1 = new int[rows1][coloumn1];
         for (int i = 0; i < rows1; i++) {
-       	 for (int j = 0; j < coloumn1; j++ ) {
-       		  mat1[i][j] = sc.nextInt();
-       	    }
+         for (int j = 0; j < coloumn1; j++ ) {
+              mat1[i][j] = sc.nextInt();
+            }
         }
        
         int rows2 = sc.nextInt();
         int coloumn2 = sc.nextInt();
         int[][] mat2 = new int[rows2][coloumn2];
         for (int i = 0; i < rows2; i++) {
-       	    for (int j = 0; j < coloumn2; j++ ) {
-       		    mat2[i][j] = sc.nextInt();
-       	    }
+            for (int j = 0; j < coloumn2; j++ ) {
+                mat2[i][j] = sc.nextInt();
+            }
         }
 
-        int[][] result = new int[rows1][coloumn1];
-        for (int i = 0; i < rows1; i++) {
-        	for (int j = 0; j < coloumn1; j++) {
-        		result[i][j] = mat1[i][j] + mat2[i][j];
-        	}
+        if ((rows1 == rows2) && (coloumn1 == coloumn2)) {
+            int[][] result = new int[rows1][coloumn1];
+            for (int i = 0; i < rows1; i++) {
+                for (int j = 0; j < coloumn1; j++) {
+                    result[i][j] = mat1[i][j] + mat2[i][j];
+                }
+            }
+        
+            for (int i = 0; i < rows1; i++) {
+                for (int j = 0; j < coloumn1; j++) {
+                    System.out.print(result[i][j]);
+                    if (j < (coloumn1 - 1)) {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            }
+        } else {
+             System.out.println("not possible");
         }
-		
-		for (int i = 0; i < rows1; i++) {
-        	for (int j = 0; j < coloumn1; j++) {
-        		System.out.print(result[i][j]);
-        		if (j < (coloumn1 - 1)) {
-        			System.out.print(" ");
-        		}
-        	}
-        	System.out.println();
-        }
-	}
+
+
+    }
 }
