580e8f358b8d316828aebc39ebbb2bd94aa29527
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-08-30 15:38:28 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-practice/m4/Assignment-2/Solution.java
+++ cspp2-practice/m4/Assignment-2/Solution.java
@@ -18,22 +18,22 @@ public final class Solution {
      *
      * @param      args  The arguments
      */
-    public static void main(final String[] args){
+    public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         int rows1 = sc.nextInt();
         int coloumn1 = sc.nextInt();
         int[][] mat1 = new int[rows1][coloumn1];
-        for (int i = 0; i < rows1; i++) {
-         for (int j = 0; j < coloumn1; j++ ) {
+        for(int i = 0; i < rows1; i++) {
+         for(int j = 0; j < coloumn1; j++ ) {
               mat1[i][j] = sc.nextInt();
             }
         }
-       
+
         int rows2 = sc.nextInt();
         int coloumn2 = sc.nextInt();
         int[][] mat2 = new int[rows2][coloumn2];
         for (int i = 0; i < rows2; i++) {
-            for (int j = 0; j < coloumn2; j++ ) {
+            for (int j = 0; j < coloumn2; j++) {
                 mat2[i][j] = sc.nextInt();
             }
         }
@@ -45,7 +45,7 @@ public final class Solution {
                     result[i][j] = mat1[i][j] + mat2[i][j];
                 }
             }
-        
+
             for (int i = 0; i < rows1; i++) {
                 for (int j = 0; j < coloumn1; j++) {
                     System.out.print(result[i][j]);
