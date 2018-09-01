bdf9eb8a0625f3ce1884afe9c0e9cc562976941d
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-09-01 10:48:39 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- cspp2-practice/m6 - Week 1 Exam/Assignment-1/Solution.java
+++ cspp2-practice/m6 - Week 1 Exam/Assignment-1/Solution.java
@@ -19,18 +19,26 @@ public final class Solution {
      * @param      n     n value
      */
     static void oddComposites(final int n) {
+        //System.out.println(n);
         
-    	for(int i = 2; i < n; i++) {
-    		if(n % i == 0) {
-    			break;
-    		}
+        for(int i = 2; i < n; i++) {
+            //System.out.println(i);
+            int c = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    c = c + 1;
+                }
+                //System.out.println(c);
+            }
 
-    		if(i % 2 != 0) {
-    		System.out.println(i);
-    	   }
-    	}
+            if(c != 2) {
+                if(i % 2 != 0) {
+                   System.out.println(i);
+                }
+            } 
+        }
 
-    	
+        
     }
     /**
     * main method as driver program.
