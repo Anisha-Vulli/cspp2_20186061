24bf39b8b02856f261382ea45fbf69f15cfcb7d0
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-09-05 20:19:50 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- cspp2-assignments/m 8/CodecampList/List.java
+++ cspp2-assignments/m 8/CodecampList/List.java
@@ -197,8 +197,9 @@ public class List {
      */
 
     public void addAll(final int[] array) {
-        for (int i = 0; i < arr.length; i++)
+        for (int i = 0; i < arr.length; i++) {
             add(arr[i]);
+        }
     }
 
     /**
