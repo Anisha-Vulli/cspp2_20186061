c52ea9096bc8c244b69b23099d824e79dbd4785d
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-09-06 11:20:10 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m 10/Assignment-2/Solution.java
+++ cspp2-assignments/m 10/Assignment-2/Solution.java
@@ -29,7 +29,7 @@ public final class Solution {
      *
      * @param      args  The arguments
      */
-	public static void main(final String[] args) {
+    public static void main(final String[] args) {
         // create an object of the list to invoke methods on it
         StringList sl = new StringList();
         StringListInterface l = (StringListInterface) sl;
@@ -45,7 +45,7 @@ public final class Solution {
             // based on the list operation invoke the corresponding method
             switch (tokens[0]) {
                 case "add":
-	                l.add(tokens[1]);
+                    l.add(tokens[1]);
                 break;
                 case "addAll":
                 if (tokens.length == 2) {
@@ -81,7 +81,5 @@ public final class Solution {
                 default:
             }
         }
-	}
+    }
 }
-
-
