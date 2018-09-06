01234b9dfba3b3b469fecc939d286d9a9fac1a00
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-09-06 11:17:49 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m 10/Assignment-2/Solution.java
+++ cspp2-assignments/m 10/Assignment-2/Solution.java
@@ -1,13 +1,31 @@
-import java.io.BufferedInputStream;
-import java.util.Scanner;
+/**
+ * Author : Anisha Vulli.
+ * Date : 06th Sept 2018.
+ */
 
+/**
+ * Bufferedinputstream import.
+ */
+import java.io.BufferedInputStream;
+
+/**
+ * Scanner method.
+ */
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
 public class Solution {
 	
-
-	public static void main(String[] args) {
+    /**
+     * Main function.
+     *
+     * @param      args  The arguments
+     */
+	public static void main(final String[] args) {
         // create an object of the list to invoke methods on it
         StringList sl = new StringList();
-        StringListInterface l = (StringListInterface)sl;
+        StringListInterface l = (StringListInterface) sl;
         //Typecasting is done for StringListInterface
         // code to read the test cases input file
         Scanner stdin = new Scanner(new BufferedInputStream(System.in));
