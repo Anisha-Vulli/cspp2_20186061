a1388c47f06020663a0cd2aa9b2aa70f4d32d9ad
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-09-06 10:22:19 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m 10/Assignment-1/List.java
+++ cspp2-assignments/m 10/Assignment-1/List.java
@@ -372,13 +372,13 @@ public class List {
                 System.out.println(l.count(Integer.parseInt(tokens[1])));
                 break;
                 case "addAll":
-                if (tokens.length == 2){
-                String[] t1 = tokens[1].split(",");
-                int temp[] = new int[t1.length];
-                for (int i = 0; i < temp.length; i++) {
-                    temp[i] = Integer.parseInt(t1[i]);
-                }
-                l.addAll(temp);
+                if (tokens.length == 2) {
+                    String[] t1 = tokens[1].split(",");
+                    int temp[] = new int[t1.length];
+                    for (int i = 0; i < temp.length; i++) {
+                        temp[i] = Integer.parseInt(t1[i]);
+                    }
+                    l.addAll(temp);
                 }
                 break;
                 case "size":
