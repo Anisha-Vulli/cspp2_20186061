b36a60745e542bf8075a6c73bfa21ab95560bcca
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-09-06 09:34:11 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m 8/Homework/List.java
+++ cspp2-assignments/m 8/Homework/List.java
@@ -261,7 +261,7 @@ public class List {
      * @param      item   The item
      */
     public void add(final int index,final int item) {
-        if (index > 0) {
+        if (index >= 0) {
             for (int i = size; i > index; i--) {
             list[i] = list[i - 1];
            }
