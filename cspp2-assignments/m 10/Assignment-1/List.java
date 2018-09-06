b5b0cf8c4a4770548b511d4fa142ee02c1beac89
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-09-06 10:24:25 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m 10/Assignment-1/List.java
+++ cspp2-assignments/m 10/Assignment-1/List.java
@@ -356,9 +356,9 @@ public class List {
             // based on the list operation invoke the corresponding method
             switch (tokens[0]) {
                case "add":
-                if ((tokens.length) == 2){
+                if ((tokens.length) == 2) {
                 String[] t = tokens[1].split(",");
-                if (t.length == 1){
+                if (t.length == 1) {
                     l.add(Integer.parseInt(tokens[1]));
                 } else {
                     if (t.length > 1) {
