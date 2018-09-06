d12f6966baba3731afa67fb2ae0aba0851836ced
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-09-06 10:37:46 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m 10/Assignment-1/List.java
+++ cspp2-assignments/m 10/Assignment-1/List.java
@@ -81,8 +81,10 @@ public class List {
      * The purpose of the constructor is to initialize the
      * class variables with some default values.
      */
-    
-    
+
+    /**
+     * Constructs the object.
+     */
 
     public List() {
 
