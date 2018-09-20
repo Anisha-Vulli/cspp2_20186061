4067e43994d92e6ac9973524021c2384217905f4
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-09-20 20:24:39 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m 23/Assignment-1/Plagarism.java
+++ cspp2-assignments/m 23/Assignment-1/Plagarism.java
@@ -7,7 +7,27 @@ public class Plagarism {
     Plagarism() {
     }
 
-    public static void freqdict(String original1, String copied1) {
+    public static String format(String buffer) {
+        String[] inputarray = buffer.split("\n");
+        Integer[] indexarray = new Integer[10];
+        for (int i = 0; i < inputarray.length; i++) {
+            if (inputarray[i].length() > 5) {
+                indexarray[i] = i;
+            }
+        }
+
+        System.out.println(Arrays.toString(indexarray));
+        return null;
+        //String input1 = str.replaceAll("[^ A-Za-z0-9]","");
+        //String original = input1.toLowerCase();
+        // String input2 = scan.nextLine().replaceAll("[^ A-Za-z0-9]","");
+        // String copied = input2.toLowerCase();
+        //freqdict(original);
+        //print();
+        //System.out.println(lcscal());
+    }
+    
+    public static void freqdict(String original1) {
         String[] originalarr = original1.split(" ");
         for (String str : originalarr) {
             int freq = 1;
@@ -22,19 +42,19 @@ public class Plagarism {
             }
         }
 
-        String[] copiedarr = copied1.split(" ");
-        for (String strco : copiedarr) {
-            int freqco = 1;
-            for (String strngco : copiedarr) {
-                if (strco.equals(strngco)) { 
-                    mapco.put(strco,freqco++); 
-                } else {
-                    if (mapco.get(strngco) == null) {
-                        mapco.put(strco, 1);
-                   } 
-                }
-            }
-        }
+        // String[] copiedarr = copied1.split(" ");
+        // for (String strco : copiedarr) {
+        //     int freqco = 1;
+        //     for (String strngco : copiedarr) {
+        //         if (strco.equals(strngco)) { 
+        //             mapco.put(strco,freqco++); 
+        //         } else {
+        //             if (mapco.get(strngco) == null) {
+        //                 mapco.put(strco, 1);
+        //            } 
+        //         }
+        //     }
+        // }
     }
 
     public static Double lcscal() {
@@ -83,18 +103,38 @@ public class Plagarism {
 
         for(File file: listFiles){
             if (file.isFile()) {
-                sa.add(file.getName());
+                sa.add(("D:\\MSIT\\IT\\cspp2_20186061\\cspp2-assignments\\m 23\\Assignment-1\\" + t + "\\" +file.getName()));
             }
         }
 
-        System.out.println(sa);
-        // Scanner scan = new Scanner(System.in);
-        // String input1 = scan.nextLine().replaceAll("[^ A-Za-z0-9]","");
-        // String input2 = scan.nextLine().replaceAll("[^ A-Za-z0-9]","");
-        // String original = input1.toLowerCase();
-        // String copied = input2.toLowerCase();
-        // freqdict(original,copied);
-        // //print();
-        // System.out.println(lcscal());
+        //System.out.println(sa);
+
+        // for (File i : sa) {
+        //     FileReader fr = new FileReader(i);
+        //     BufferedReader br = new BufferedReader(fr);
+        //     String buffer = "";
+        //     String s = "";
+        //     while (((s = br.readLine()) != null)) {
+        //         buffer += s;
+        //         System.out.println(buffer);
+        //     }
+        // }
+        try {
+            String s1 = "";
+            for (String i : sa) {
+                File file = new File(i);
+                Scanner input = new Scanner(file);
+                while (input.hasNextLine()) {
+                    s1 += input.nextLine();
+                    s1 += " ";
+                }
+            } 
+            format(s1);
+            //System.out.println(s1);
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+
+        // format(buffer);
     }
 }
