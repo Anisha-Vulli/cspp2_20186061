efd3b3aeec5a50f2d8dac6cd944a39f1116625e4
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-09-21 17:58:55 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m 23/Assignment-1/Plagarism.java
+++ cspp2-assignments/m 23/Assignment-1/Plagarism.java
@@ -146,13 +146,13 @@ public class Plagarism {
         for (int k = 0; j < n; j++) {
             System.out.print("\t" + list.get(k));
         }
-        int k = 0;
+        int p = 0;
         for (int i = 0; i < values.size(); i++) {
             if (j == n) {
                 j = 0;
                 System.out.println("");
-                System.out.print(list.get(k));
-                k++;
+                System.out.print(list.get(p));
+                p++;
             }
             System.out.print("\t\t" + values.get(i));
             j++;
