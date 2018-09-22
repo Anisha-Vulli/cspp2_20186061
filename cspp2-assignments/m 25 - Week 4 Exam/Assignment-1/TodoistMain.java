f7f77c09ff709c7943b9c5014922b019cb0b9767
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-09-22 12:10:58 +0530

    "check_style_score -> 3 of 14 passed. style score: 0/1 "

--- cspp2-assignments/m 25 - Week 4 Exam/Assignment-1/TodoistMain.java
+++ cspp2-assignments/m 25 - Week 4 Exam/Assignment-1/TodoistMain.java
@@ -124,7 +124,8 @@ class Todoist {
 
     public String toString() {
         for (Task str : tasks) {
-             System.out.println(str.gettitle() + ", " + str.getname() + ", " + str.gettime() +", " + str.getimportant() + ", " + str.geturgent() +", " + str.getstatus());
+            //System.out.println(str.gettitle() + ", " + str.getname() + ", " + str.gettime() +", " + str.getimportant() + ", " + str.geturgent() +", " + str.getstatus());
+            return str.gettitle() + ", " + str.getname() + ", " + str.gettime() +", " + str.getimportant() + ", " + str.geturgent() +", " + str.getstatus();
         }
         return null;
     }
