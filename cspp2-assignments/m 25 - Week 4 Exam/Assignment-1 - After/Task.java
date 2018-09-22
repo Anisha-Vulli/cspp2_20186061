8566dd3943af1e2a18dc8679299bb3c0155aa65e
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-09-22 17:26:08 +0530

    "check_style_score -> 0 of 14 passed. style score: 0/1 "

--- cspp2-assignments/m 25 - Week 4 Exam/Assignment-1 - After/Task.java
+++ cspp2-assignments/m 25 - Week 4 Exam/Assignment-1 - After/Task.java
@@ -5,22 +5,22 @@ class Task {
     private boolean important;
     private boolean urgent;
     private String status;
-    Task(String title1, String assignedTo1, int timeToComplete1, boolean important1, boolean urgent1, String status1) {
+    Task(String title1, String assignedTo1, int timeToComplete1, boolean important1, boolean urgent1, String status1) throws Exception{
         this.title = title1;
         this.assignedTo = assignedTo1;
         this.timeToComplete = timeToComplete1;
         this.important = important1;
         this.urgent = urgent1;
         this.status = status1;
-        // if (title.equals(" ")) {
-        //  throw new Exception("Title not provided");
-        // }
-        // if (timeToComplete < 0) {
-        //  throw new Exception("Invalid timeToComplete" + timeToComplete);
-        // }
-        // if (!status.equals("todo") || !status.equals("done")) {
-        //  throw new Exception("Invalid status" + status);
-        // }
+        if (title.equals(" ")) {
+         throw new Exception("Title not provided");
+        }
+        if (timeToComplete < 0) {
+         throw new Exception("Invalid timeToComplete" + timeToComplete);
+        }
+        if (!status.equals("todo") || !status.equals("done")) {
+         throw new Exception("Invalid status" + status);
+        }
     }
 
     // public String checktitle(String title) {
