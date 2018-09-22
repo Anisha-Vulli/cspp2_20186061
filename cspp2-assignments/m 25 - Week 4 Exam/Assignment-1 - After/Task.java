51aa1b6314d7dc9b83e9b3d3dc8b4c1d93850b9b
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-09-22 17:14:32 +0530

    "check_style_score -> 1 of 14 passed. style score: 0/1 "

---
+++ cspp2-assignments/m 25 - Week 4 Exam/Assignment-1 - After/Task.java
@@ -0,0 +1,45 @@
+class Task {
+    private String title;
+    private String assignedTo;
+    private int timeToComplete;
+    private boolean important;
+    private boolean urgent;
+    private String status;
+    Task(String title1, String assignedTo1, int timeToComplete1, boolean important1, boolean urgent1, String status1) {
+        this.title = title1;
+        this.assignedTo = assignedTo1;
+        this.timeToComplete = timeToComplete1;
+        this.important = important1;
+        this.urgent = urgent1;
+        this.status = status1;
+        // if (title.equals(" ")) {
+        //  throw new Exception("Title not provided");
+        // }
+        // if (timeToComplete < 0) {
+        //  throw new Exception("Invalid timeToComplete" + timeToComplete);
+        // }
+        // if (!status.equals("todo") || !status.equals("done")) {
+        //  throw new Exception("Invalid status" + status);
+        // }
+    }
+
+    public String checkimportant(boolean imp) {
+        if (important) {
+            return "Important";
+        } else {
+            return "Not important";
+        }
+    }
+
+    public String checkurgent(boolean urg) {
+        if (urgent) {
+            return "Urgent";
+        } else {
+            return "Not urgent";
+        }
+    }
+
+    public String toString() {
+        return title + ", " + assignedTo + ", " + timeToComplete + ", " + checkimportant(important) + ", " + checkurgent(urgent) + ", " + status;
+    }
+}
