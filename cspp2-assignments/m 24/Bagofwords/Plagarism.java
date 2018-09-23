89cef32d04bc92c03d2db4945a5843b055f3fffd
Anisha-Vulli <bunnysmart.007@msitprogram.net>
2018-09-23 09:55:54 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m 24/Bagofwords/Plagarism.java
+++ cspp2-assignments/m 24/Bagofwords/Plagarism.java
@@ -171,7 +171,7 @@ class Plagarism {
         //System.out.println(list);
         int n = list.size();
         int j = 0;
-        System.out.print("\t       ");
+        System.out.print(" \t       ");
         for (int k = 0; j < n; j++) {
             System.out.print(list.get(k) + "\t");
             k++;
